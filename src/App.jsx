0a97807aeb9f69facbd5ecec96f6c6f58efd3850
Idah Abubakar Isah <idahabubakar49@gmail.com>
2024-12-28 14:46:39 +0100

    Template created

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from "react-router-dom";
+import SignUp from "./auth/SignUp";
+import SignIn from "./auth/SignIn";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      {/* Public Routes */}
+      <Route
+        path="signup"
+        element={
+            <SignUp />
+        }
+      />
+      <Route
+        path="sign-in"
+        element={
+            <SignIn />
+        }
+      />
+      <Route path="*" element={<Navigate to="/sign-in" />} />
+    </Route>
+  ))
+
+
+  const App = () => {
+    return <RouterProvider router={router} />;
+  };
+  
+  export default App;
